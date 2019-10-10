d57c6efbf67b9a226fc7ab73a1abc26976c659a4
Piotr <piotr43a@gmail.com>
2019-10-10 23:11:33 +0200

    change connectivity interceptor to inject connectivity manager

--- app/src/main/java/com/piotr/androidtemplate/base/network/interceptor/ConnectivityInterceptor.kt
+++ app/src/main/java/com/piotr/androidtemplate/base/network/interceptor/ConnectivityInterceptor.kt
@@ -1,17 +1,16 @@
 package com.piotr.androidtemplate.base.network.interceptor
 
-import android.content.Context
 import android.net.ConnectivityManager
 import com.piotr.androidtemplate.base.network.NoInternetConnectionException
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
 
-class ConnectivityInterceptor(private val context: Context) : Interceptor {
+class ConnectivityInterceptor(private val connectivityManager: ConnectivityManager) : Interceptor {
 
   @Throws(IOException::class)
   override fun intercept(chain: Interceptor.Chain): Response {
-    if (!isOnline(context)) {
+    if (!isOnline(connectivityManager)) {
       throw NoInternetConnectionException()
     }
 
@@ -20,11 +19,6 @@ class ConnectivityInterceptor(private val context: Context) : Interceptor {
     return chain.proceed(builder.build())
   }
 
-  private fun isOnline(context: Context): Boolean {
-    val connectivityManager = context.getSystemService(
-      Context.CONNECTIVITY_SERVICE
-    ) as ConnectivityManager// TODO change to kodein
-    val netInfo = connectivityManager.activeNetworkInfo
-    return netInfo != null && netInfo.isConnected
-  }
+  private fun isOnline(connectivityManager: ConnectivityManager) =
+    connectivityManager.activeNetworkInfo?.isConnected ?: false
 }
